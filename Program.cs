618f84835cb32b6f5c2690ee4b6de1324485ffed
Nik Schaad <nikschaad@gmail.com>
2022-09-23 13:37:46 +0200

    Added handler

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+namespace Fiary
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Handler handler = new Handler();
+
+            if (args[0] == null)
+            {
+                Console.WriteLine("No file selected");
+                return;
+            }
+
+            handler.ParseFile(args[0]);
+        }
+    }
+}
