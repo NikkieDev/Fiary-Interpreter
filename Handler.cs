f0798cac493da9e0349fdc68c01c5670582384d9
Nik Schaad <nikschaad@gmail.com>
2022-09-23 13:37:53 +0200

    Added handler

---
+++ Handler.cs
@@ -0,0 +1,12 @@
+using System.Text.Json;
+
+namespace Fiary
+{
+    class Handler
+    {
+        internal void ParseFile(String FileName) // Possible filetype later
+        {
+            
+        }
+    }
+}
