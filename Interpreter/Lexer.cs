fc172c2a52f3f091056cfe5d0c85ff21f0de4da7
Nikkie Schaad <Nikkieschaad@gmail.com>
2022-09-25 14:34:46 +0200

    I am now too confused for my own code

--- Interpreter/Lexer.cs
+++ Interpreter/Lexer.cs
@@ -2,10 +2,38 @@ namespace Fiary
 {
     class Lexer
     {
+        private enum TokenType
+        {
+            STRING_START,
+            SPACE,
+            STRING_END,
+            VALUE_DECLARE,
+            VALUE,
+            VALUE_NAME
+        }
         internal String[] Interpret(String[] Data)
         {
             List<String> InterpretedList = new List<String>();
 
+            String _char = "";
+            TokenType Token = new TokenType();
+
+            for (int i = 0; i < InterpretedList.Count; i++)
+            {
+                _char = InterpretedList[i];
+
+                switch (_char)
+                {
+                    case "\"":
+                        Token = TokenType.STRING_START;
+                        break;
+                    case " ":
+                        Token = TokenType.SPACE;
+                        break;
+                }
+
+                
+            }
 
             String[] InterpretedData = InterpretedList.ToArray();
             return InterpretedData;
