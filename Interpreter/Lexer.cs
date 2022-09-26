ecbeada1b8ab910cc9efc384fee61eb910672d67
Nikkie Schaad <Nikkieschaad@gmail.com>
2022-09-26 19:02:13 +0200

    home

--- Interpreter/Lexer.cs
+++ Interpreter/Lexer.cs
@@ -2,37 +2,15 @@ namespace Fiary
 {
     class Lexer
     {
-        private enum TokenType
-        {
-            STRING_START,
-            SPACE,
-            STRING_END,
-            VALUE_DECLARE,
-            VALUE,
-            VALUE_NAME
-        }
         internal String[] Interpret(String[] Data)
         {
             List<String> InterpretedList = new List<String>();
 
             String _char = "";
-            TokenType Token = new TokenType();
 
             for (int i = 0; i < InterpretedList.Count; i++)
             {
-                _char = InterpretedList[i];
-
-                switch (_char)
-                {
-                    case "\"":
-                        Token = TokenType.STRING_START;
-                        break;
-                    case " ":
-                        Token = TokenType.SPACE;
-                        break;
-                }
-
-                
+                _char = InterpretedList[i];    
             }
 
             String[] InterpretedData = InterpretedList.ToArray();
