b374d22632ff28fd63aa75dcfe495b9bdd154f41
Nik Schaad <nikschaad@gmail.com>
2022-09-27 12:18:35 +0200

    commit

--- Interpreter/Lexer.cs
+++ Interpreter/Lexer.cs
@@ -1,38 +1,18 @@
+using System;
+
 namespace Fiary
 {
     class Lexer
     {
-        internal static List<String> mem = new List<String>();
-        internal String[] Interpret(String[] Data)
+        internal List<String> Interpret(String[] Data)
         {
-            List<String> InterpretedList = new List<String>();
-
-            String _char = "";
-
-            for (int i = 0; i < InterpretedList.Count; i++)
+            List<String> InterpretedData = new List<String>();
+            
+            for (int i = 0; i < Data.Length; i++)
             {
-                switch (_char)
-                {
-                    case "\"":
-                        int QuoteAmount = 0;
-
-                        for (int i = 0; i < mem.Count; i++)
-                        {
-                            if (mem[i])
-                                QuoteAmount++;
-                        }
-
-                        int _QuoteAmountResult = QuoteAmount / 2;
-                        String QuoteAmountResult = _QuoteAmountResult.ToString();
-
-                        if (QuoteAmountResult.EndsWith(".5"))
-                        {
-                            
-                        }
-                }
+                
             }
 
-            String[] InterpretedData = InterpretedList.ToArray();
             return InterpretedData;
         }
     }
